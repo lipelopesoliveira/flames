00102ec00d0de16682b4528215e44b09e8acba0f
Felipe Lopes de Oliveira <lipe.lopes.oliveira@gmail.com>
2026-07-22 22:29:05 +0200

    Add example

---
+++ examples/Advanced/01-Hybrid_GCMC_MD_mixed_calculators/run_hybrid.py
@@ -0,0 +1,91 @@
+import json
+
+import ase
+from ase.constraints import FixBondLengths
+from ase.data import vdw_radii
+from ase.io import read
+from mace.calculators import mace_mp
+from numba import get_num_threads, set_num_threads
+
+from flames.gcmc import GCMC
+from flames.lennard_jones import CustomLennardJones
+from flames.utilities import read_cif
+
+NUM_THREADS_TO_USE = 1
+set_num_threads(NUM_THREADS_TO_USE)
+
+print(get_num_threads())
+
+with open("DREIDING.json", "r") as f:
+    lj_params = json.loads(f.read())
+
+calc1 = CustomLennardJones(lj_params, vdw_cutoff=12.8, shifted=True)
+
+calc2 = mace_mp(
+    model="medium-0b2",
+    dispersion=False,
+    damping="zero",  # choices: ["zero", "bj", "zerom", "bjm"]
+    dispersion_xc="pbe",
+    default_dtype="float32",
+    device="cuda",
+)
+
+# Load the framework structure
+framework: ase.Atoms = read_cif("GZU-1.cif")  # type: ignore
+
+# Load the adsorbate structure
+adsorbate: ase.Atoms = read("ch4.xyz")  # type: ignore
+
+c = FixBondLengths([[0, 1], [0, 2], [0, 3], [0, 4]])
+adsorbate.set_constraint(c)
+
+Temperature = 198.0  # in Kelvin
+pressure = 1_000_000  # in Pa = 1 bar
+MCSteps = 3_00
+MDSteps = 3_000
+
+print(
+    f"Running GCMC simulation for pressure: {pressure:.2f} Pa at temperature: {Temperature:.2f} K"
+)
+
+gcmc = GCMC(
+    model=calc1,
+    framework_atoms=framework,
+    adsorbate_atoms=adsorbate,
+    temperature=Temperature,
+    pressure=pressure,
+    device="cpu",
+    vdw_radii=vdw_radii,
+    vdw_factor=0.6,
+    save_frequency=1,
+    debug=False,
+    output_to_file=True,
+    cutoff_radius=8.0,
+    automatic_supercell=True,
+    LLM=False,
+    move_weights={
+        "insertion": 0.25,
+        "deletion": 0.25,
+        "translation": 0.25,
+        "rotation": 0,
+        "reinsertion": 0.25,
+    },
+)
+
+gcmc.logger.print_header()
+
+for j in range(5):
+    gcmc.run(MCSteps)
+    gcmc.npt(
+        nsteps=MDSteps,
+        time_step=0.5,
+        mode="aniso_flex",
+        calculator=calc2,
+        movie_interval=1,
+        output_interval=1000,
+    )
+
+
+gcmc.run(MCSteps)
+gcmc.logger.print_summary()
+gcmc.save_results()
